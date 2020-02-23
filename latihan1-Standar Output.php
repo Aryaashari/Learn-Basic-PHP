e8705facdbf7e5badd674d313936fda9eb8fefa5
Arya Ashari <61337332+Aryaashari@users.noreply.github.com>
2020-02-23 19:52:10 +0800

    Add files via upload

---
+++ latihan1-Standar Output.php
@@ -0,0 +1,15 @@
+<?php
+
+//Standar Output
+//1. echo = Untuk Menampilkan Tulisan, Isi Variable d.l.l
+//2. print = Untuk Menampilkan Tulisan, Isi Variable d.l.l
+//3. print_r = Menampilkan isi array
+//4. var_dump = Menampilkan tipe data
+	
+
+echo "Arya Ashari";
+print(100);
+var_dump(12);
+
+?>
+
